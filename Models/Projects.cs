02e753a3cca053a5cb0cf9b337e3d7fc64af8324
Qiaoyi <37761636+li992@users.noreply.github.com>
2020-09-03 18:58:42 -0600

    20200903

--- Models/Projects.cs
+++ Models/Projects.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,10 +11,27 @@ namespace YoMateProjectShare.Models
     public class Projects
     {
         public int Id { get; set; }
+
+        [Display(Name = "Article")]
+        [StringLength(200, MinimumLength = 3)]
         public string ArticleName { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Author")]
         public string AuthorName { get; set; }
+        
+        [Display(Name ="Upload Time")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime UploadTime { get; set; }
+
+
         public string AbstractText { get; set; }
     }
+    
+    public class ProjectsDBContext : DbContext
+    {
+        public DbSet<Projects> projects { get; set; }
+    }
 }
+
---
+++ Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoMateProjectShare.Data;
+
+namespace YoMateProjectShare.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new YoMateProjectShareContext(
+                serviceProvider.GetRequiredService<DbContextOptions<YoMateProjectShareContext>>()))
+            {
+                if (context.Projects.Any())
+                {
+                    return;
+                }
+
+                context.Projects.AddRange(
+                    new Projects
+                    {
+                        ArticleName = "Example 1",
+                        AuthorName = "Example Author 1",
+                        UploadTime = DateTime.Parse("2020-9-3"),
+                        AbstractText = "This is an example"
+                    },
+
+                    new Projects
+                    {
+                        ArticleName = "Example 2",
+                        AuthorName = "Example Author 2",
+                        UploadTime = DateTime.Parse("2020-9-2"),
+                        AbstractText = "This is an example"
+                    },
+
+                    new Projects
+                    {
+                        ArticleName = "Example 3",
+                        AuthorName = "Example Author 23",
+                        UploadTime = DateTime.Parse("2020-9-1"),
+                        AbstractText = "This is an example"
+                    },
+
+                    new Projects
+                    {
+                        ArticleName = "Example 4",
+                        AuthorName = "Example Author 4",
+                        UploadTime = DateTime.Parse("2020-8-31"),
+                        AbstractText = "This is an example"
+                    },
+
+                    new Projects
+                    {
+                        ArticleName = "Example 5",
+                        AuthorName = "Example Author 5",
+                        UploadTime = DateTime.Parse("2020-8-30"),
+                        AbstractText = "This is an example"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
