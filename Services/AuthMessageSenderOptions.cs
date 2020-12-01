c3b94a0752d9920da89d7f34a3a5316fa2e698c6
Qiaoyi <37761636+li992@users.noreply.github.com>
2020-12-01 15:54:16 -0600

    update key

--- Services/AuthMessageSenderOptions.cs
+++ Services/AuthMessageSenderOptions.cs
@@ -8,6 +8,6 @@ namespace YoMateProjectShare.Services
     public class AuthMessageSenderOptions
     {
         public string SendGridUser { get { return "Yomate Admin"; } }
-        public string SendGridKey { get { return "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"; } }
+        public string SendGridKey { get { return "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"; } }
     }
 }
